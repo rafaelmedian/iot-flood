44161af23de2fe01f9fbcdae8e3a6dd5d8040846
Luis <webdeveloperpr@gmail.com>
2017-10-07 01:07:30 -0400

    Display flooding data as circles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker, withScriptjs } from "react-google-maps";
+import {
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  withScriptjs,
+  Circle
+} from "react-google-maps";
 import './App.css';
+
+const positions = [
+  {
+    lat: 18.3978791,
+    lng: -66.08799
+  },
+  {
+    lat: 18.3978793,
+    lng: -66.08775
+  },
+  {
+    lat: 18.3978797,
+    lng: -66.08793
+  },
+  {
+    lat: 18.3978789,
+    lng: -66.08797
+  },
+];
+
+const startLocation = { lat: 18.3978789, lng: -66.08797 };
+const defaultZoom = 20;
+
+const mapCircles = positions => {
+  return positions.map((pos, i) => {
+    return (
+      <Circle
+        key={i}
+        clickable
+        center={pos}
+        radius={2}
+        onCenterChanged={this.onCenterChanged}
+        onRadiusChanged={this.onRadiusChanged}
+        options={{
+          fillColor: '#f00',
+          strokeColor: '#f00',
+        }}
+      />
+    )
+  });
+};
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultZoom={defaultZoom}
+    defaultCenter={startLocation}
   >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }}/>}
+    {mapCircles(positions)}
   </GoogleMap>
 ));
 
@@ -18,10 +65,10 @@ class App extends Component {
         <MyMapComponent
           isMarkerShown
           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />      </div>
+          loadingElement={<div style={{ height: `100%` }}/>}
+          containerElement={<div style={{ height: `100vh` }}/>}
+          mapElement={<div style={{ height: `100%` }}/>}
+        /></div>
     );
   }
 }
