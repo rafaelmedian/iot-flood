3ebe9e15a0ccde56418b75bd504467692f397e7d
Luis <webdeveloperpr@gmail.com>
2017-10-07 01:49:52 -0400

    Update circles and radiuses

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {
   withGoogleMap,
   GoogleMap,
-  Marker,
   withScriptjs,
   Circle
 } from "react-google-maps";
@@ -10,25 +9,57 @@ import './App.css';
 
 const positions = [
   {
-    lat: 18.3978791,
-    lng: -66.08799
+    lat: 18.3973291,
+    lng: -66.08799,
+    depth: 1
   },
   {
-    lat: 18.3978793,
-    lng: -66.08775
+    lat: 18.3978593,
+    lng: -66.08775,
+    depth: 1
   },
   {
-    lat: 18.3978797,
-    lng: -66.08793
+    lat: 18.3973797,
+    lng: -66.08793,
+    depth: 3
   },
   {
-    lat: 18.3978789,
-    lng: -66.08797
+    lat: 18.3978489,
+    lng: -66.08797,
+    depth: 4
   },
 ];
 
 const startLocation = { lat: 18.3978789, lng: -66.08797 };
+
 const defaultZoom = 20;
+
+const colors = {
+  red: '#f00',
+  blue: '#0708ff',
+  green: '#50ff00',
+  orange: '#ff6500'
+};
+
+const circleColor = (depth => {
+  let color = colors.green;
+  if (depth === 1) color = colors.green;
+  if (depth === 2) color = colors.blue;
+  if (depth === 3) color = colors.orange;
+  if (depth === 4) color = colors.red;
+
+  return {
+    fillColor: color,
+    strokeColor: color,
+  };
+});
+
+const circleRadius = (radius => {
+  if (radius === 1) return 2;
+  if (radius === 2) return 3;
+  if (radius === 3) return 4;
+  if (radius === 4) return 5;
+});
 
 const mapCircles = positions => {
   return positions.map((pos, i) => {
@@ -37,13 +68,10 @@ const mapCircles = positions => {
         key={i}
         clickable
         center={pos}
-        radius={2}
+        radius={circleRadius(pos.depth)}
         onCenterChanged={this.onCenterChanged}
         onRadiusChanged={this.onRadiusChanged}
-        options={{
-          fillColor: '#f00',
-          strokeColor: '#f00',
-        }}
+        options={circleColor(pos.depth)}
       />
     )
   });
@@ -68,7 +96,8 @@ class App extends Component {
           loadingElement={<div style={{ height: `100%` }}/>}
           containerElement={<div style={{ height: `100vh` }}/>}
           mapElement={<div style={{ height: `100%` }}/>}
-        /></div>
+        />
+      </div>
     );
   }
 }
